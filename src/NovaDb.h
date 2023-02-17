688403085e0b4411f4b54ad23dcd0e6aa05f72e7
jsueprez <ajperez502@gmail.com>
2023-02-17 17:57:22 +0000

    add filereader class

---
+++ src/NovaDb.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <json/json.h>
+
+struct NovaLocker
+{
+    int id {};
+    std::string type {};
+    bool locked {};
+    int pinNumber {};
+    // ...
+};
+
+class NovaDb
+{
+public:
+    NovaDb(const std::string &p_filesystem);
+    ~NovaDb()
+    {
+        if(m_ifs.is_open()) { m_ifs.close();}
+    }
+
+    int get_total_novaLocker() const ;
+    NovaLocker get_novaLocker_info(int p_id) const;
+
+private:
+    std::string m_filename;
+    std::ifstream m_ifs;
+    std::vector<NovaLocker> m_novaLockers;
+
+    int read();
+
+
+
+};
